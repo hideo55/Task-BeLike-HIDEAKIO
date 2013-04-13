9987772c8cb334b85d0c9bdb56e2685ca920130f
hideo55 <hide.o.j55@gmail.com>
2013-04-13 11:41:25 +0900

    initial commit

---
+++ lib/Task/BeLike/HIDEAKIO.pm
@@ -0,0 +1,39 @@
+package Task::BeLike::HIDEAKIO;
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+Task::BeLike::HIDEAKIO - [One line description of module's purpose here]
+
+=head1 SYNOPSIS
+
+  use strict;
+  use warnings;
+  use Task::BeLike::HIDEAKIO;
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=head1 SEE ALSO
+
+=head1 AUTHOR
+
+Hideaki Ohno  E<lt>hide.o.j55 {at} gmail.comE<gt>
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
---
+++ t/00_compile.t
@@ -0,0 +1,7 @@
+use Test::More tests => 1;
+
+BEGIN {
+use_ok( 'Task::BeLike::HIDEAKIO' );
+}
+
+diag( "Testing Task::BeLike::HIDEAKIO $Task::BeLike::HIDEAKIO::VERSION" );
